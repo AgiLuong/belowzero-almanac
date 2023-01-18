43075a9554306dcf988d2018dc342a19ba02de01
Agi Luong <xwq5ja@virginia.edu>
2023-01-18 15:16:24 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.almanac {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.almanac to javafx.fxml;
+    exports com.example.almanac;
+}
