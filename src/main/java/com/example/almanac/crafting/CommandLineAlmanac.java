862461c0ef95ad5ee5a9ace35d68e9f8aa1af214
Agi Luong <xwq5ja@virginia.edu>
2023-02-01 17:10:18 -0500

    started a class for getting recipes

---
+++ src/main/java/com/example/almanac/crafting/CommandLineAlmanac.java
@@ -0,0 +1,12 @@
+package com.example.almanac.crafting;
+
+import com.example.almanac.database.DatabaseManager;
+
+public class CommandLineAlmanac {
+
+    private DatabaseManager manager;
+
+    public CommandLineAlmanac() {
+        this.manager = new DatabaseManager();
+    }
+}
