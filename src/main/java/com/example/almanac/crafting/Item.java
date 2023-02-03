9e7d35fc115c161cc2215c187ac70000885be0eb
Agi Luong <xwq5ja@virginia.edu>
2023-02-03 13:59:13 -0500

    changed some fields for Item to better represent recipes:

--- src/main/java/com/example/almanac/crafting/Item.java
+++ src/main/java/com/example/almanac/crafting/Item.java
@@ -1,16 +1,17 @@
 package com.example.almanac.crafting;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Item {
     private String name;
-    private boolean isRaw;
-    private ArrayList<Item> recipe;
+    private Boolean isRaw;
+    private HashMap<Item, Integer> recipe;
 
     public Item(String name, boolean isRaw) {
         this.name = name;
         this.isRaw = isRaw;
-        this.recipe = new ArrayList<>();
+        this.recipe = new HashMap<Item, Integer>();
     }
     public String getName() {
         return name;
@@ -18,18 +19,19 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
-
-    public boolean isRaw() {
-        return isRaw;
-    }
-    public void setRaw(boolean raw) {
-        isRaw = raw;
-    }
-
-    public ArrayList<Item> getRecipe() {
+    public HashMap<Item, Integer> getRecipe() {
         return recipe;
     }
-    public void addRecipe(Item need) {
-        this.recipe.add(need);
+    public void addRecipe(Item need, int quantity) {
+        this.recipe.put(need, quantity);
+    }
+    public Boolean getRaw() {
+        return isRaw;
+    }
+    public void setRaw(Boolean raw) {
+        this.isRaw = raw;
+    }
+    public boolean equal(Item other) {
+        return Objects.equals(this.name, other.name);
     }
 }
