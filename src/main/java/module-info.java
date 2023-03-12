52f317440cc0dc15edea228c36967a46e020ac1d
Agi Luong <xwq5ja@virginia.edu>
2023-03-11 22:00:16 -0500

    some javafx config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,6 @@ module com.example.almanac {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-//    requires eu.hansolo.tilesfx;
-//    requires com.almasb.fxgl.all;
     requires java.sql;
     requires org.xerial.sqlitejdbc;
 
