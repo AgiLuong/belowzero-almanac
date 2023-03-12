b72053e58b1d73a8d676eb1e0062a330ab91208b
Agi Luong <xwq5ja@virginia.edu>
2023-03-12 01:48:10 -0500

    got the textfield to register enter key and record what is in it

--- src/main/java/com/example/almanac/HelloController.java
+++ src/main/java/com/example/almanac/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.almanac;
 
+import com.example.almanac.crafting.CommandLineAlmanac;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    private CommandLineAlmanac cla = new CommandLineAlmanac();
     @FXML
-    private Label welcomeText;
-
+    private TextField input;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void myPrint(ActionEvent ae) {
+        System.out.println(input.getText());
     }
 }
